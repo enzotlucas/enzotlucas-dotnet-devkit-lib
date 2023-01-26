60bdd8fea10953dd598ca233cc4a66a2006895b7
enzotlucas <enzotlucas31@gmail.com>
2023-01-26 09:16:32 -0300

    adding initial fixtures and test classes

---
+++ src/enzotlucas.DevKit.Tests/Core/Entities/BaseEntityTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enzotlucas.DevKit.Tests.Core.Entities
+{
+    public class BaseEntityTests
+    {
+
+    }
+}
