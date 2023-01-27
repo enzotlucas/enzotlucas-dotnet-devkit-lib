92d962f7aab4c1d54b30edda1961b2d72b823a3a
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-01-27 18:54:25 -0300

    adding unit tests to extenions

---
+++ src/enzotlucas.DevKit.Tests/Extensions/RequestExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using enzotlucas.DevKit.Extensions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+
+namespace enzotlucas.DevKit.Tests.Extensions
+{
+    public class RequestExtensionsTests
+    {
+        [Fact]
+        public void GetCorrelationId_ExistingCorrelationId_ShouldReturnExistingCorrelationId()
+        {
+            //Arrange
+            var correlationId = Guid.NewGuid();
+            var request = Substitute.For<HttpRequest>();
+            request.Headers["x-correlation-id"] = correlationId.ToString();
+
+            //Act
+            var response = request.GetCorrelationId();
+
+            //Assert
+            response.Should().Be(correlationId);
+        }
+
+        [Fact]
+        public void GetCorrelationId_NonExistingCorrelationId_ShouldReturnEmptyGuid()
+        {
+            //Arrange
+            var request = Substitute.For<HttpRequest>();
+
+            //Act
+            var response = request.GetCorrelationId();
+
+            //Assert
+            response.Should().BeEmpty();
+        }
+    }
+}
