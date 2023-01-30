d23f4e8178b8a90ec54cad0f7ecb330444edc10b
enzotlucas <enzotlucas31@gmail.com>
2023-01-30 18:36:53 -0300

    exclude swagger from code coverage

--- src/enzotlucas.DevKit.Tests/Middlewares/DevKitErrorHandlerMiddlewareTests.cs
+++ src/enzotlucas.DevKit.Tests/Middlewares/DevKitErrorHandlerMiddlewareTests.cs
@@ -2,14 +2,7 @@
 
 namespace enzotlucas.DevKit.Tests.Middlewares
 {
-    [Collection(nameof(DevKitFixtureCollection))]
     public class DevKitErrorHandlerMiddlewareTests
     {
-        private readonly DevKitFixture _fixture;
-
-        public DevKitErrorHandlerMiddlewareTests(DevKitFixture fixture)
-        {
-            _fixture = fixture;
-        }
     }
 }
--- src/enzotlucas.DevKit/DependencyInjection/ApiSpecification/Swagger/SwaggerDefaultValues.cs
+++ src/enzotlucas.DevKit/DependencyInjection/ApiSpecification/Swagger/SwaggerDefaultValues.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Diagnostics.CodeAnalysis;
 
 namespace enzotlucas.DevKit.ApiSpecification.Swagger
 {
+    [ExcludeFromCodeCoverage]
     internal sealed class SwaggerDefaultValues : IOperationFilter
     {
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
